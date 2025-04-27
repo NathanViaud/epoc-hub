2a3e4c5ebc5655c1c07fcb633109f7b9c879b3ca
VIAUD Nathan <nathan.viaud@inria.fr>
2025-04-27 15:55:57 +0200

    improvement: calculate used quota by user folder size

--- server/utils/files.ts
+++ server/utils/files.ts
@@ -1,35 +1,5 @@
-import type { BlobSize, PowOf2, SizeUnit } from "@nuxthub/core";
+export async function getUserQuota(id: string) {
+    const { blobs } = await hubBlob().list({ prefix: id });
 
-export async function getUserQuota(id: number) {
-    const epocs = await useDrizzle().select().from(tables.epocs).where(eq(tables.epocs.user, id));
-
-    let totalSize = 0;
-
-    for (const epoc of epocs) {
-        const file = await hubBlob().head(epoc.file);
-        const size = file.size;
-        totalSize += size;
-    }
-
-    return totalSize;
-}
-
-export function getMaxBlobSize(bytes: number): BlobSize {
-    const units: SizeUnit[] = ["B", "KB", "MB", "GB"];
-    const powOf2: PowOf2[] = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
-
-    let size = bytes;
-    let unitIndex = 0;
-
-    while (size >= 1024 && unitIndex < units.length - 1) {
-        size /= 1024;
-        unitIndex++;
-    }
-
-    const maxPow = powOf2.reduce((prev, curr) => {
-        if (curr <= size) return curr;
-        return prev;
-    }, 1 as PowOf2);
-
-    return `${maxPow}${units[unitIndex]}` as BlobSize;
+    return blobs.reduce((acc, blob) => acc + blob.size, 0);
 }
