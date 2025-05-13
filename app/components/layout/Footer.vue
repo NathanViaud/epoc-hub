1b6b69d87b97e29ecb4d455cb713b3b2bf8b9bab
VIAUD Nathan <nathan.viaud@inria.fr>
2025-05-13 15:36:28 +0200

    style: add heart

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -1,7 +1,15 @@
 <template>
     <footer class="flex items-center justify-between border-t border-(--ui-border) py-8">
-        <span class="text-sm text-muted">
-            Made with ❤️
+        <span class="text-sm text-muted flex items-center gap-1">
+            <span>Made with </span>
+            <div class="relative">
+                <UIcon name="i-lucide-heart" class="[&>path]:fill-red-500 [&>path]:stroke-red-500" mode="svg" />
+                <UIcon
+                    name="i-lucide-heart"
+                    class="[&>path]:fill-red-500 [&>path]:stroke-red-500 absolute animate-ping top-0"
+                    mode="svg"
+                />
+            </div>
             <NuxtLink href="https://github.com/NathanViaud">Nathan Viaud</NuxtLink>
         </span>
         <nav>
